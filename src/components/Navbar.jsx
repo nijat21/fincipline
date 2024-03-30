6506b942b74721c800dabfdb338128a135221b21
Nijat Ismayilov <nijat.ismayilov@outlook.com>
2024-03-30 08:04:00 +0000

    deploy v3

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -82,8 +82,6 @@ function Navbar() {
                                         <button className="btn border border-black text-black dark:border-white dark:text-white py-1 px-3 w-[90px] md:ml-8 rounded md:static">
                                             <Link to={'/login'}>Log In</Link>
                                         </button>
-                                    </li>
-                                    <li className="my-7 md:my-0 flex justify-center" onClick={() => setIsOpen(false)}>
                                         <button className="btn border border-black bg-black dark:border-white dark:bg-white dark:text-black text-white w-[90px] py-1 px-3 md:ml-3 rounded md:static">
                                             <Link to={'/signup'}>Sign Up</Link>
                                         </button>
