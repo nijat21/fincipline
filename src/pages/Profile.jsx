3663aca8547c51f4b398b01b3d68a1b3d6f90c50
Nijat Ismayilov <nijat.ismayilov@outlook.com>
2024-06-28 05:42:09 +0400

    Profile styles wip

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -43,39 +43,47 @@ function Profile() {
 
     return (
         <div className="flex flex-col h-screen items-center justify-center">
-            <div className="flex items-center justify-center w-80 md:w-1/2 px-8 py-10 rounded-md shadow-2xl bg-blue-800
-            border border-zinc-300 dark:border-slate-800">
+            <div className="flex items-center justify-center w-80 md:w-1/2 px-8 py-10 rounded-lg shadow-2xl 
+            bg-gradient-to-r from-slate-400 to-slate-300
+            dark:from-indigo-800 dark:to-indigo-500">
                 <div className="flex flex-col items-center justify-center text-lg w-full">
-                    <div className="max-w-40  flex flex-col items-center justify-center border rounded-sm hover:bg-slate-600 cursor-pointer"
+                    <div className={`max-w-40 md:max-w-52 flex flex-col items-center justify-center border cursor-pointer shadow-md
+                    ${imgUrl ? "border-transparent" : "border-black dark:border-slate-300 hover:border-transparent"} `}
                         onClick={() => { navigate('/profile/upload'); }}>
                         {imgUrl ?
-                            <div className="relative flex justify-center items-end group">
-                                <img src={imgUrl} alt="" className='' />
+                            <div className="relative text-slate-300 flex justify-center items-end group">
+                                <img src={imgUrl} alt="" className='rounded-md' />
                                 <p className="absolute w-full text-lg text-center place-content-center py-2 bg-black bg-opacity-80 hidden group-hover:inline-block">
                                     Upload a new photo
                                 </p>
                             </div>
                             :
-                            <div className="p-4">
+                            <div className="p-4 flex flex-col justify-center items-center hover:bg-slate-700 hover:text-slate-300">
                                 <i className="fa-regular fa-user fa-2xl p-10"></i>
-                                <p className="text-md">Upload a photo</p>
+                                <p className="text-sm">Upload a photo</p>
                             </div>
                         }
                     </div>
 
-                    <h1 className="font-medium text-xl h-16 flex items-center ">{`${name[0].toUpperCase()}${user && name.slice(1)}`}</h1>
-                    <button className="w-full p-2 m-2 border rounded-sm border-black dark:border-slate-300 hover:bg-neutral-700 hover:text-white
-                    dark:hover:bg-white dark:hover:text-black  hover:border-transparent cursor-pointer"
-                        onClick={() => setShowDetailsEdit(true)}>
-                        Name & email</button>
-                    {/* Add some edit icon to make obvious */}
-                    <button className="w-full p-2 m-2 border border-black dark:border-slate-300 hover:bg-neutral-700 hover:text-white
-                    dark:hover:bg-white dark:hover:text-black  hover:border-transparent cursor-pointer"
-                        onClick={() => setShowPasswordEdit(true)}>
-                        Update password</button>
-                    <button className="w-full p-2 m-2 border border-black bg-neutral-500 text-white dark:border-slate-300 hover:bg-neutral-700 hover:text-white
-                    dark:hover:bg-white dark:hover:text-black  hover:border-transparent cursor-pointer"
-                        onClick={handleDelete}>Delete account</button>
+                    <h3 className="font-medium h-16 flex items-center ">{`${name[0].toUpperCase()}${user && name.slice(1)}`}</h3>
+                    <div className="w-40 md:w-52  box-border flex flex-col items-center">
+                        <p className="text-lg px-2 border-b border-black dark:border-slate-300">Update:</p>
+
+                        <button className="w-full mt-2 rounded-sm hover:bg-slate-700 hover:text-white
+                        font-semibold dark:hover:bg-white dark:hover:text-black cursor-pointer
+                        border border-black dark:border-slate-300 md:border-transparent md:dark:border-transparent"
+                            onClick={() => setShowDetailsEdit(true)}>
+                            Name & Email</button>
+                        {/* Add some edit icon to make obvious */}
+                        <button className="w-full mb-2 m-1 rounded-sm  hover:bg-slate-700 hover:text-white
+                        font-semibold dark:hover:bg-white dark:hover:text-black cursor-pointer
+                        border border-black dark:border-slate-300 md:border-transparent md:dark:border-transparent"
+                            onClick={() => setShowPasswordEdit(true)}>
+                            Password</button>
+                        <button className="w-full p-2 border border-black dark:border-slate-300 hover:bg-slate-700 hover:text-white
+                         dark:hover:bg-white dark:hover:text-black  hover:border-transparent bg-black bg-opacity-60 md:bg-transparent bg-opacity-1"
+                            onClick={handleDelete}>Delete account</button>
+                    </div>
                 </div>
                 {showPasswordEdit &&
                     <EditPassword onClose={() => setShowPasswordEdit(false)} />
@@ -84,7 +92,7 @@ function Profile() {
                     <EditUserDetails onClose={() => setShowDetailsEdit(false)} onUpdate={handleUserDetailsUpdate} />
                 }
             </div>
-        </div>
+        </div >
     );
 }
 
